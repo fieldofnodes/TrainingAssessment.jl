d53ee56b3a650abf258fbf978e471869c921e140
Documenter.jl <documenter@juliadocs.github.io>
2024-06-03 23:59:52 +0000

    build based on 83ba3af

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrainingAssessment","category":"page"},{"location":"#TrainingAssessment","page":"Home","title":"TrainingAssessment","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrainingAssessment.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrainingAssessment]","category":"page"}]
+}
